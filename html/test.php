41a096423370eaba8ea37b4a55d0d2f1aefc3cdb
TS System <system@total-service.co.jp>
2016-03-16 09:08:09 +0900

    htmlディレクトリ整理

--- html/test.php
+++
@@ -1,7 +0,0 @@
-<meta charset="UTF-8">
-<title>テスト</title>
-<?php
-$db = new PDO('mysql:host=mysql;dbname=test', 'root', 'pass');
-$db->query("INSERT INTO test VALUES('テスト太郎')");
-$st = $db->query("SELECT * FROM test");
-var_dump($st->fetchAll());
